774b91dc9d87696e0492b4aad0989c516774fe52
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-15 00:52:43 +0200

    Feat: add Featured component to home/

---
+++ frontend/src/components/Featured.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Featured = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+      {/* California */}
+      <div className="relative w-full text-left">
+        <figure className="relative">
+          <img
+            src="/nature3.jpg"
+            alt="Beautiful nature scene in California"
+            className="w-full aspect-[4/3] object-cover rounded-md"
+          />
+          <figcaption className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/70 to-transparent text-white p-4 rounded-b-md">
+            <h1 className="text-2xl font-bold">California</h1>
+            <h2 className="text-lg">123 properties</h2>
+          </figcaption>
+        </figure>
+      </div>
+
+      {/* North Carolina */}
+      <div className="relative w-full text-left">
+        <figure className="relative">
+          <img
+            src="/nature2.jpg"
+            alt="Beautiful nature scene in North Carolina"
+            className="w-full aspect-[4/3] object-cover rounded-md"
+          />
+          <figcaption className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/70 to-transparent text-white p-4 rounded-b-md">
+            <h1 className="text-2xl font-bold">North Carolina</h1>
+            <h2 className="text-lg">234 properties</h2>
+          </figcaption>
+        </figure>
+      </div>
+
+      {/* Dublin */}
+      <div className="relative w-full text-left">
+        <figure className="relative">
+          <img
+            src="/nature.jpg"
+            alt="Beautiful nature scene in Dublin"
+            className="w-full aspect-[4/3] object-cover rounded-md"
+          />
+          <figcaption className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black/70 to-transparent text-white p-4 rounded-b-md">
+            <h1 className="text-2xl font-bold">Dublin</h1>
+            <h2 className="text-lg">456 properties</h2>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
 import Search from "../../components/Search";
+import Featured from "../../components/Featured";
 
 const Home = () => {
   return (
     <div className="h-[120vh] bg-[#edfbff] pt-[10vh]">
       <Navbar />
       <Header />
+      <div className="pt-[10vh] w-[90%] lg:w-[80%] xl:w-[70%] mx-auto flex flex-col  gap-5">
+        <Featured />
+
+        <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">
+          Browse by property type
+        </h2>
+      </div>
     </div>
   );
 };
