0816026853b2123754b83338f5c4a1e25f5801ea
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-20 02:11:54 +0200

    Feat: Add backend routes and models basic structure and hotel model structure

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import { db } from './dbConnection.js';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+import hotelRoutes from './routes/hotel.route.js'
+import roomRoutes from './routes/rooms.route.js';
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.get("/", (req, res) => res.json({ message: "Welcome to EasyBookings" }))
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/hotels", hotelRoutes);
+app.use("/api/rooms", roomRoutes);
+
+app.get("/", (req, res) => res.json({ message: "Welcome to EasyBookings" }));
+
 db.then(() => {
     app.listen(PORT, () => console.log(`server running at ${PORT}`));
 });
