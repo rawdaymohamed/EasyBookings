3f58b4c512c91e1abb3352354104f2d33ccc91e9
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-24 05:32:37 +0200

    Feat: Add room Model

--- backend/models/room.model.js
+++ backend/models/room.model.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const roomSchema = new Schema({
-
-}, { timestamps: true });
-export default mongoose.model('Room', roomSchema);
+const roomModel = new Schema({
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    maxPeople: { type: Number, required: true },
+    roomNumbers: [{ Number: Number, unavailableDates: { type: [Date] }, }]
+},
+    { timestamps: true }
+);
+export default mongoose.model('Room', roomModel);
