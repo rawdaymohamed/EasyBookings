6ef912920d6aecb47212a2381fbb03cba8f11b98
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-02 00:16:29 +0200

    Fix: fix router route imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
