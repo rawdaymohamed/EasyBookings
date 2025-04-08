710c9ca8a1e4e70f34d6e95b3b409db9e878f169
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-04-08 21:26:50 +0200

    Feat: Add getCountByCity controller and route

--- backend/routes/hotel.route.js
+++ backend/routes/hotel.route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { create, edit, getAll, remove, get } from "../controllers/hotel.controller.js";
+import { create, edit, getAll, remove, get, getCountByCity, getCountByType } from "../controllers/hotel.controller.js";
 import { verifyAdmin } from "../utils/auth.js";
 const router = Router();
+router.get("/countByCity", getCountByCity);
+router.get("/countByType", getCountByType);
 router.put("/:id", verifyAdmin, edit);
 router.delete("/:id", verifyAdmin, remove);
 router.get("/:id", get);
