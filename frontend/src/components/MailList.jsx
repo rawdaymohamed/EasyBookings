94c622ae29a492206ea73a88f30f8df77a2dfccd
Rawda Yasser <rawdaymohamed@gmail.com>
2025-09-12 16:02:30 +0300

    Fix: Fix mail list component email input style

--- frontend/src/components/MailList.jsx
+++ frontend/src/components/MailList.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const MailList = () => {
   return (
-    <div className="bg-black w-full py-16 md:py-18">
-      <div className="w-[90%] lg:w-[40%] mx-auto text-center text-white">
+    <div className="bg-gray-800 w-full py-16 md:py-20">
+      <div className="w-[90%] lg:w-[40%] mx-auto text-center text-purple-50 flex flex-col gap-4">
+  
+        <div className="flex flex-col gap-2">
         {/* Title */}
         <h2 className="text-xl sm:text-2xl md:text-3xl font-bold">
           Save Time, Save Money!
@@ -12,6 +14,7 @@ const MailList = () => {
         <p className="text-sm sm:text-base md:text-lg mt-2">
           Sign up and we'll send you the best deals for you.
         </p>
+        </div>
         {/* Sign-up Form */}
         <div className="mt-6 flex flex-col sm:flex-row justify-center items-center gap-4">
           <input
@@ -19,7 +22,7 @@ const MailList = () => {
             name="email"
             id="email"
             placeholder="Enter your email"
-            className="w-full sm:flex-grow px-4 py-3 rounded-lg text-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-400"
+            className="w-full sm:flex-grow px-4 py-3 rounded-lg text-gray-300 focus:outline-none ring-1 ring-purple-400 focus:ring-2 focus:ring-purple-500"
           />
           <button className="w-full sm:w-auto bg-purple-400 hover:bg-purple-500 text-black font-semibold px-6 py-3 rounded-lg transition duration-300">
             Subscribe
